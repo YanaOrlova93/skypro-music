2d5ba89bee18c4c0b655828dda6cbf7bcc66bdb4
YanaOrlova93 <orlova93zm@mail.ru>
2024-01-25 21:50:21 +0300

    стили nav

--- skypro-music/src/components/NavMenu/NavMenu.jsx
+++ skypro-music/src/components/NavMenu/NavMenu.jsx
@@ -1,46 +1,45 @@
-import { useState } from 'react';
-import './NavMenu.css'
-import {styled} from 'styled-components'
-  
+import { useState } from "react";
+import * as S from '.navMenu.styles.js';
+import React from 'react';
 
-const StyledMainNav = styled.nav`
-width: 244px;
-    background-color: #181818;
-    padding: 20px 0 20px 36px;
-`
-
-function NavMenu () {
+export const  NavMenu = () => {
 
   const [visible, setVisible] = useState(false);
   const toggleVisibility = () => setVisible(!visible);
 
-    return (
-        <StyledMainNav className="main__nav nav">
-            <div className="nav__logo logo">
-              <img className="logo__image" src="./assets/logo/logo.png" alt="logo" />
-            </div>
-            <div className="nav__burger burger" onClick={toggleVisibility}>
-              <span className="burger__line"/>
-              <span className="burger__line"/>
-              <span className="burger__line"/>
-            </div>
-            {visible && (
-            <div className="nav__menu menu">
-              <ul className="menu__list">
-                <li className="menu__item">
-                  <a href="http://" className="menu__link">Главное </a>
-                </li>
-                <li className="menu__item">
-                  <a href="http://" className="menu__link">Мой плейлист </a>
-                </li>
-                <li className="menu__item">
-                  <a href="../signin.html" className="menu__link">Войти</a>
-                </li>
-              </ul>
-            </div>
-            )}
-          </StyledMainNav>
-    )
+  return (
+    <S.MainNav>
+      <S.NavLogo>
+        <S.LogoImage src="./assets/logo/logo.png" alt="logo" />
+      </S.NavLogo>
+      <S.NavBurger onClick={toggleVisibility}>
+        <S.BurgerLine></S.BurgerLine>
+        <S.BurgerLine></S.BurgerLine>
+        <S.BurgerLine></S.BurgerLine>
+      </S.NavBurger>
+      {visible && (
+        <S.NavMenu>
+          <S.MenuList>
+            <S.MenuItem >
+              <S.MenuLink href="http://" >
+                Главное{" "}
+              </S.MenuLink>
+            </S.MenuItem>
+            <S.MenuItem >
+              <S.MenuLink href="http://" >
+                Мой плейлист{" "}
+              </S.MenuLink>
+            </S.MenuItem>
+            <S.MenuItem >
+              <S.MenuLink href="../signin.html">
+                Войти
+              </S.MenuLink>
+            </S.MenuItem>
+          </S.MenuList>
+        </S.NavMenu>
+      )}
+    </S.MainNav>
+  );
 }
 
 export default NavMenu;
