498ed260506479ff1931177899b37130d7e8282f
YanaOrlova93 <orlova93zm@mail.ru>
2024-02-15 19:39:36 +0300

    rjvvbn

--- skypro-music/src/components/PopupMenuButtons/PopupMenuButtons.styles.js
+++ skypro-music/src/components/PopupMenuButtons/PopupMenuButtons.styles.js
@@ -1,5 +1,58 @@
 // import { styled } from 'styled-components';
-import styled, { css } from 'styled-components';
+// import styled, { css } from 'styled-components';
+// export const CenterblockFilter = styled.div`
+//     display: -webkit-box;
+//     display: -ms-flexbox;
+//     display: flex;
+//     -webkit-box-orient: horizontal;
+//     -webkit-box-direction: normal;
+//     -ms-flex-direction: row;
+//     flex-direction: row;
+//     -webkit-box-align: center;
+//     -ms-flex-align: center;
+//     align-items: center;
+//     margin-bottom: 51px;
+// `
+
+// export const FilterTitle = styled.div`
+//     font-style: normal;
+//     font-weight: 400;
+//     font-size: 16px;
+//     line-height: 24px;
+//     margin-right: 15px;
+// `
+
+// export const FilterContent = styled.div`
+//     position: relative;
+//     &:not(:last-child) {
+//         margin-right: 10px;
+//     }
+// `
+
+// export const FilterButton = styled.button`
+//     font-style: normal;
+//     font-weight: 400;
+//     font-size: 16px;
+//     line-height: 24px;
+//     border-radius: 60px;
+//     padding: 6px 20px;
+//     &:hover {
+//         border-color: #d9b6ff;
+//         color: #d9b6ff;
+//         cursor: pointer;
+        
+//     }
+    
+//     ${props => props.isActive && css`
+//     color: #d9b6ff; // Цвет текста для активной кнопки
+//     border-color: #ad61ff; // Цвет границы для активной кнопки
+//     cursor: pointer;
+//   `}
+// `;
+
+
+import { styled } from 'styled-components';
+
 export const CenterblockFilter = styled.div`
     display: -webkit-box;
     display: -ms-flexbox;
@@ -29,7 +82,7 @@ export const FilterContent = styled.div`
     }
 `
 
-export const FilterButton = styled.button`
+export const FilterButton = styled.div`
     font-style: normal;
     font-weight: 400;
     font-size: 16px;
@@ -40,16 +93,9 @@ export const FilterButton = styled.button`
         border-color: #d9b6ff;
         color: #d9b6ff;
         cursor: pointer;
-        
     }
-    
-    ${props => props.isActive && css`
-    color: #d9b6ff; // Цвет текста для активной кнопки
-    border-color: #ad61ff; // Цвет границы для активной кнопки
-    cursor: pointer;
-  `}
-`;
-
-
-
+    border: ${(props) =>
+        props.$isActive ? '1px solid #ad61ff' : '1px solid #ffffff'};
+    color: ${(props) => (props.$isActive ? '#ad61ff' : '')};
+`
   
--- skypro-music/src/components/PopupMenuList/AuthorList.styles.js
+++ skypro-music/src/components/PopupMenuList/AuthorList.styles.js
@@ -1,42 +1,75 @@
 import { styled } from 'styled-components';
-// import 'overlayscrollbars/overlayscrollbars.css';
+import 'overlayscrollbars/overlayscrollbars.css';
 
 export const PopupList = styled.ul`
+    // display: flex;
+    // flex-direction: column;
+    // gap: 28px;
+    // background-color: #313131;
+    // color: #fff;
+    // max-height: 305px;
+    // width: 248px;
+    // box-sizing: border-box;
+    // padding: 10px;
+   
+    // border-radius: 12px;
+    // position: absolute;
+    // top: 35px;
+    // left: 0;
+    // overflow-y: scroll;
+
+    // &::-webkit-scrollbar {
+    //     width: 8px;
+    //   }
+    
+    //   &::-webkit-scrollbar-track {
+    //     -webkit-box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.2) inset;
+    //     background-color: #f9f9fd;
+    //     border-radius: 10px;
+    //   }
+    
+    //   &::-webkit-scrollbar-thumb {
+    //     border-radius: 10px;
+    //     background: linear-gradient(315deg, rgb(22, 123, 217), rgb(89, 20, 146));
+    //   }
+    
+    //   &:hover {
+    //     border: 1px solid #9A48F1;
+    //   }
+     
+
+    // 
     display: flex;
     flex-direction: column;
     gap: 28px;
     background-color: #313131;
     color: #fff;
-    max-height: 305px;
-    width: 248px;
+    max-height: 250px;
+    max-width: 400px;
     box-sizing: border-box;
-    padding: 34px;
-    border: 1px solid #FFF;
+    padding: 10px;
     border-radius: 12px;
+    z-index: 2;
     position: absolute;
-    top: 35px;
     left: 0;
+    overflow: scroll;
+    margin-top: 10px;
+    overflow-x: hidden;
     overflow-y: scroll;
-
     &::-webkit-scrollbar {
-        width: 8px;
-      }
-    
-      &::-webkit-scrollbar-track {
-        -webkit-box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.2) inset;
-        background-color: #f9f9fd;
-        border-radius: 10px;
-      }
-    
-      &::-webkit-scrollbar-thumb {
-        border-radius: 10px;
-        background: linear-gradient(315deg, rgb(22, 123, 217), rgb(89, 20, 146));
-      }
-    
-      &:hover {
-        border: 1px solid #9A48F1;
-      }
-     
+          width: 6px;
+        }
+      
+        &::-webkit-scrollbar-track {
+          -webkit-box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.2) inset;
+          background-color: #f9f9fd;
+          border-radius: 10px;
+        }
+      
+        &::-webkit-scrollbar-thumb {
+          border-radius: 10px;
+          background: linear-gradient(315deg, rgb(22, 123, 217), rgb(89, 20, 146));
+        }
       
     `;
 
