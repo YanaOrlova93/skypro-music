4b639b1deab132da2e0c9def85f0d5c74d8f6db3
YanaOrlova93 <orlova93zm@mail.ru>
2024-03-06 18:37:58 +0300

    redux6

--- skypro-music/src/pages/SignIn/SignIn.jsx
+++ skypro-music/src/pages/SignIn/SignIn.jsx
@@ -47,6 +47,127 @@
 //     )
 //   }
 
+// import React from 'react';
+// import * as S from './SignIn.styles.js';
+// import { GlobalStyle } from '../../components/Global.styles/Global.styles.js';
+// import { useRef, useState } from 'react';
+// import { UserContext } from '../../Authorization.jsx';
+// import { useContext } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { getToken,login } from '../../Api.jsx';
+
+
+// export const Login = () => {
+
+//   const navigate = useNavigate()
+
+//   const [email, setEmail] = useState('')
+//     const [password, setPassword] = useState('')
+//     const [error, setError] = useState('')
+//     const [userData, setUserData] = useContext(UserContext)
+
+//     const signInButtonRef = useRef(null)
+
+//     async function handleSignIn({ email, password }) {
+//       if (email === '') {
+//           setError('Укажите почту')
+//           return
+//       }
+//       if (password === '') {
+//           setError('Укажите пароль')
+//           return
+//       }
+
+//       try {
+//           const response = await fetch(
+//               'https://skypro-music-api.skyeng.tech/user/login/',
+//               {
+//                   method: 'POST',
+//                   body: JSON.stringify({
+//                       email: email,
+//                       password: password,
+//                   }),
+//                   headers: {
+//                       'content-type': 'application/json',
+//                   },
+//               },
+//           )
+
+//           if (response.status === 400) {
+//               setError(
+//                   'Произошла ошибка с данными. Неверные логин или пароль',
+//               )
+//               return
+//           } else if (response.status === 401) {
+//               setError('Пользователь с таким email или паролем не найден')
+//               return
+//           } else if (response.status === 500) {
+//               setError('Сервер не отвечает, попробуй позже')
+//               return
+//           }
+
+//           const data = await response.json()
+//           setUserData(data)
+//           setUserData(data.username)
+//           localStorage.setItem('user', JSON.stringify(userData))
+//             navigate('/')
+//       } catch (error) {
+//           console.log(error)
+//       }
+//   }
+//     return(
+//     <>
+//       <GlobalStyle />
+//       <S.Wrapper>
+//         <S.ContainerEnter>
+//           <S.ModalBlock>
+//             <S.ModalFormLogin action="#">
+//             <S.ModalButtonLink to="/">
+//                 <S.ModalLogo>
+//                   <img src="../img/logo_modal.png" alt="logo" />
+//                 </S.ModalLogo>
+//             </S.ModalButtonLink>
+//               <S.ModalInputLogin
+//                 className="login"
+//                 type="text"
+//                 name="login"
+//                 placeholder="Почта"
+//                 value={email}
+//                                 onChange={(event) => {
+//                                     setEmail(event.target.value)
+//                                 }}
+//               />
+//               <S.ModalInput
+//                 className="password"
+//                 type="password"
+//                 name="password"
+//                 placeholder="Пароль"
+//                 value={password}
+//                 onChange={(event) => {
+//                     setPassword(event.target.value)
+//                 }}
+//             />
+//             {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
+              
+//               <S.ModalBtnEnter  >
+//               <S.ModalButtonLink  ref={signInButtonRef}
+//                                 onClick={() => {
+//                                     signInButtonRef.current.disabled = true
+//                                     handleSignIn({ email, password })
+//                                 }}>Войти</S.ModalButtonLink>
+//               </S.ModalBtnEnter>
+//               <S.ModalBtnSignUp>
+//               <S.ModalButtonLink to="/SignUp">Зарегистрироваться</S.ModalButtonLink>
+//               </S.ModalBtnSignUp>
+//             </S.ModalFormLogin>
+//           </S.ModalBlock>
+//         </S.ContainerEnter>
+//       </S.Wrapper>
+//     </>
+//     )
+//   }
+
+
 import React from 'react';
 import * as S from './SignIn.styles.js';
 import { GlobalStyle } from '../../components/Global.styles/Global.styles.js';
@@ -54,19 +175,17 @@ import { useRef, useState } from 'react';
 import { UserContext } from '../../Authorization.jsx';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getToken,login } from '../../Api.jsx';
 
 export const Login = () => {
 
   const navigate = useNavigate()
-
   const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const [userData, setUserData] = useContext(UserContext)
-
-    const signInButtonRef = useRef(null)
-
-    async function handleSignIn({ email, password }) {
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [, setUserData] = useContext(UserContext)
+  const signInButtonRef = useRef(null)
+  async function handleSignIn({ email, password }) {
       if (email === '') {
           setError('Укажите почту')
           return
@@ -75,44 +194,36 @@ export const Login = () => {
           setError('Укажите пароль')
           return
       }
-
-      try {
-          const response = await fetch(
-              'https://skypro-music-api.skyeng.tech/user/login/',
-              {
-                  method: 'POST',
-                  body: JSON.stringify({
-                      email: email,
-                      password: password,
-                  }),
-                  headers: {
-                      'content-type': 'application/json',
-                  },
-              },
-          )
-
-          if (response.status === 400) {
-              setError(
-                  'Произошла ошибка с данными. Неверные логин или пароль',
-              )
-              return
-          } else if (response.status === 401) {
-              setError('Пользователь с таким email или паролем не найден')
-              return
-          } else if (response.status === 500) {
-              setError('Сервер не отвечает, попробуй позже')
-              return
-          }
-
-          const data = await response.json()
-          setUserData(data)
-          setUserData(data.username)
-          localStorage.setItem('user', JSON.stringify(userData))
-            navigate('/')
-      } catch (error) {
-          console.log(error)
+      signInButtonRef.current.disabled = true
+      const response = await login({ email, password })
+      if (response.status === 400) {
+          setError('Произошла ошибка с данными. Неверные логин или пароль')
+          signInButtonRef.current.disabled = false
+          return
+      } else if (response.status === 401) {
+          setError('Пользователь с таким email или паролем не найден')
+          signInButtonRef.current.disabled = false
+          return
+      } else if (response.status === 500) {
+          setError('Сервер не отвечает, попробуй позже')
+          signInButtonRef.current.disabled = false
+          return
       }
+      const data = await response.json()
+      setUserData(data.username)
+      localStorage.setItem('user', JSON.stringify(data.username))
+      signInButtonRef.current.disabled = false
+      navigate('/')
+
+      const tokenResponse = await getToken({ email, password })
+      const tokens = await tokenResponse.json()
+      const accessToken = tokens.access
+      const refreshToken = tokens.refresh
+
+      localStorage.setItem('accessToken', accessToken)
+      localStorage.setItem('refreshToken', refreshToken)
   }
+    
     return(
     <>
       <GlobalStyle />
--- skypro-music/src/pages/SignUp/SignUp.jsx
+++ skypro-music/src/pages/SignUp/SignUp.jsx
@@ -41,6 +41,128 @@
 //     )
 //   }
 
+// import React from 'react';
+// import * as S from './SignUp.slyles.js';
+// import { GlobalStyle } from '../../components/Global.styles/Global.styles.js';
+// import { useState, useRef } from 'react';
+// import { UserContext } from '../../Authorization.jsx';
+// import { useContext } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { register, getToken } from '../../Api.jsx';
+
+// export const SignUp = () => {
+//   const navigate = useNavigate()
+
+//   const [email, setEmail] = useState('')
+//     const [password, setPassword] = useState('')
+//     const [repeatPassword, setRepeatPassword] = useState('')
+//     const [error, setError] = useState('')
+//     const [userData, setUserData] = useContext(UserContext)
+//     const signUpButtonRef = useRef(null)
+//     async function handleSignUp({ email, password }) {
+//         if (email === '') {
+//             setError('Укажите почту')
+//             return
+//         }
+//         if (password === '') {
+//             setError('Укажите пароль')
+//             return
+//         }
+//         if (password !== repeatPassword) {
+//             setError('Пароли не совпадают')
+//             return
+//         }
+
+//         try {
+//             const response = await fetch(
+//                 'https://skypro-music-api.skyeng.tech/user/signup/',
+//                 {
+//                     method: 'POST',
+//                     body: JSON.stringify({
+//                         email: email,
+//                         password: password,
+//                         username: email,
+//                     }),
+//                     headers: {
+//                         'content-type': 'application/json',
+//                     },
+//                 },
+//             )
+
+//             if (response.status === 400) {
+//                 setError(
+//                     'Вы вели неверные данные',
+//                 )
+//                 return
+//             } else if (response.status === 500) {
+//                 setError('Сервер не отвечает, попробуй позже')
+//                 return
+//             }
+
+//             const data = await response.json()
+//             setUserData(data.username)
+//             localStorage.setItem('user', JSON.stringify(userData))
+//             navigate('/')
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+    
+//     return(
+//     <>
+//       <GlobalStyle />
+//       <S.Wrapper>
+//         <S.ContainerEnter >
+//           <S.ModalBlock>
+//             <S.ModalFormLogin action="#">
+//             <S.ModalButtonLink to="/">
+//                 <S.ModalLogo>
+//                   <img src="../img/logo_modal.png" alt="logo" />
+//                 </S.ModalLogo>
+//             </S.ModalButtonLink>
+//               <S.ModalInput
+//                 type="text"
+//                 name="login"
+//                 placeholder="Почта"
+//                 value={email}
+//                 onChange={(event) => {
+//                     setEmail(event.target.value)
+//                 }}
+//               />
+//               <S.ModalInput
+//                 type="password"
+//                 name="password"
+//                 placeholder="Пароль"
+//                 value={password}
+//                                 onChange={(event) => {
+//                                     setPassword(event.target.value)
+//                                 }}
+//               />
+//               <S.ModalInput
+//                 type="password"
+//                 name="password"
+//                 placeholder="Повторите пароль"
+//                 value={repeatPassword}
+//                                 onChange={(event) => {
+//                                     setRepeatPassword(event.target.value)
+//                                 }}
+//                             />
+//                             {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
+//               <S.ModalBtnEnter >
+//               <S.ModalButtonLink ref={signUpButtonRef}
+//                                 onClick={() => {
+//                                     signUpButtonRef.current.disabled = true
+//                                     handleSignUp({ email, password })
+//                                 }}>Зарегистрироваться</S.ModalButtonLink>
+//               </S.ModalBtnEnter>
+//             </S.ModalFormLogin>
+//           </S.ModalBlock>
+//         </S.ContainerEnter>
+//       </S.Wrapper>
+//     </>
+//     )
+//   }
+
 import React from 'react';
 import * as S from './SignUp.slyles.js';
 import { GlobalStyle } from '../../components/Global.styles/Global.styles.js';
@@ -48,18 +170,16 @@ import { useState, useRef } from 'react';
 import { UserContext } from '../../Authorization.jsx';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { register, getToken } from '../../Api.jsx';
 
 export const SignUp = () => {
   const navigate = useNavigate()
-
-  const [email, setEmail] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [repeatPassword, setRepeatPassword] = useState('')
     const [error, setError] = useState('')
-    const [userData, setUserData] = useContext(UserContext)
-
+    const [, setUserData] = useContext(UserContext)
     const signUpButtonRef = useRef(null)
-
     async function handleSignUp({ email, password }) {
         if (email === '') {
             setError('Укажите почту')
@@ -73,40 +193,32 @@ export const SignUp = () => {
             setError('Пароли не совпадают')
             return
         }
-
-        try {
-            const response = await fetch(
-                'https://skypro-music-api.skyeng.tech/user/signup/',
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: email,
-                        password: password,
-                        username: email,
-                    }),
-                    headers: {
-                        'content-type': 'application/json',
-                    },
-                },
+        signUpButtonRef.current.disabled = true
+        const response = await register({ email, password })
+        if (response.status === 400) {
+            setError(
+                'Произошла ошибка с данными. Попробуйте изменить почту или пароль',
             )
-
-            if (response.status === 400) {
-                setError(
-                    'Вы вели неверные данные',
-                )
-                return
-            } else if (response.status === 500) {
-                setError('Сервер не отвечает, попробуй позже')
-                return
-            }
-
-            const data = await response.json()
-            setUserData(data.username)
-            localStorage.setItem('user', JSON.stringify(userData))
-            navigate('/')
-        } catch (error) {
-            console.log(error)
+            signUpButtonRef.current.disabled = false
+            return
+        } else if (response.status === 500) {
+            setError('Сервер не отвечает, попробуй позже')
+            signUpButtonRef.current.disabled = false
+            return
         }
+        const data = await response.json()
+        setUserData(data.username)
+        localStorage.setItem('user', JSON.stringify(data.username))
+        signUpButtonRef.current.disabled = false
+        navigate('/')
+
+        const tokenResponse = await getToken({ email, password })
+        const tokens = await tokenResponse.json()
+        const accessToken = tokens.access
+        const refreshToken = tokens.refresh
+
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refreshToken)
     }
     
     return(
