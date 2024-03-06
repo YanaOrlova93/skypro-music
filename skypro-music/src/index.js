9e2c0e4f6972e6fde43d0a6ebd3b1771be946a09
YanaOrlova93 <orlova93zm@mail.ru>
2024-03-06 13:43:55 +0300

    redux4

--- skypro-music/src/index.js
+++ skypro-music/src/index.js
@@ -1,15 +1,37 @@
 
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import {App} from './App.jsx';
+// import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+// <React.StrictMode>
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// </React.StrictMode>
+// );
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/index.js'
+import { IndexStyles } from './index.styles.js'
+import { BrowserRouter } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-<React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</React.StrictMode>
-);
+    <>
+        <IndexStyles />
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
+    </>,
+)
